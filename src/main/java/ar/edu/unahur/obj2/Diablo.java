a53ced0b17fa874f391b18daf206c7d07ea1baa2
AlmironLeandro <leandroealmiron@hotmail.com>
2019-10-10 19:10:04 -0300

    completo con fallas

--- src/main/java/ar/edu/unahur/obj2/Diablo.java
+++ src/main/java/ar/edu/unahur/obj2/Diablo.java
@@ -6,7 +6,20 @@ import java.util.Comparator;
 public class Diablo {
     ArrayList<Demonio> listaDeDemonios = new ArrayList<Demonio>();
     public void totalDeAlmasCazadas(){listaDeDemonios.forEach(demonio-> demonio.almasDeTodosLosLugares());}
-    public Demonio obtenerDemonio(){listaDeDemonios.stream().filter(demonio -> demonio.cantidad())}
+    public Demonio obtenerDemonio(){
+        Demonio demonioMax=listaDeDemonios.get(1);
+        Demonio conMayorAlmas = demonioMax;
+        for(int i=0;i<listaDeDemonios.size()-1;i++) {
+        Demonio d1 = listaDeDemonios.get(i);
+        if(demonioMax.getCantidad()>d1.getCantidad()){
+            conMayorAlmas=demonioMax;
+        }
+        else {
+            conMayorAlmas=d1;
+        }
+    }
+    return conMayorAlmas;
+    }
     public Alma laMasValiente(){
        Alma almaMax=listaDeDemonios.get(1).almasMasValiente();
         for(int i=1;i<listaDeDemonios.size()-1;i++){
